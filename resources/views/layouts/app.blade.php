50afa6b342fe3f8906eb9efa2d173dea64749cb4
singhWaraich <singh4narender@gmail.com>
2020-09-23 11:26:29 +0530

    change path

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,17 +7,17 @@
     <meta name="description" content="Online Bill Pay">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('public/css/app.css') }}">
   <!--   <link rel="apple-touch-icon" href="apple-icon.png"> -->
    <!--  <link rel="shortcut icon" href="favicon.ico"> -->
-    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/css/font-awesome.min.css') }}">
     <!-- <link rel="stylesheet" href="{{ asset('css/themify-icons.css') }}"> -->
-    <link rel="stylesheet" href="{{ asset('css/design.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/toastr.min.css') }}">
-    <script src="{{ asset('jquery/jquery-3.2.1.min.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('public/css/design.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/css/custom.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/css/responsive.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/css/toastr.min.css') }}">
+    <script src="{{ asset('public/jquery/jquery-3.2.1.min.js') }}"></script>
     <script src="https://unpkg.com/gijgo@1.9.11/js/gijgo.min.js" type="text/javascript"></script>
     <link href="https://unpkg.com/gijgo@1.9.11/css/gijgo.min.css" rel="stylesheet" type="text/css" />
 </head>
@@ -48,10 +48,10 @@
     @endif
   </div>
 </div>
-<script src="{{ asset('bootstrap/js/popper.min.js') }}"></script>
-<script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('js/custom.js') }}"></script>
-<script src="{{ asset('js/toastr.min.js') }}"></script>
+<script src="{{ asset('public/bootstrap/js/popper.min.js') }}"></script>
+<script src="{{ asset('public/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('public/js/custom.js') }}"></script>
+<script src="{{ asset('public/js/toastr.min.js') }}"></script>
 {!! Toastr::message() !!}
     </body>
 </html>  
