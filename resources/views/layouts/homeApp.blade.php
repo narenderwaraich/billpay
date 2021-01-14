6a361765c5b9b89817860e64b8edfcb8064720ff
singhWaraich <singh4narender@gmail.com>
2021-01-14 18:23:57 +0530

    track code add

--- resources/views/layouts/homeApp.blade.php
+++ resources/views/layouts/homeApp.blade.php
@@ -8,8 +8,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <link rel="stylesheet" type="text/css" href="/public/css/app.css">
-  <!--   <link rel="apple-touch-icon" href="apple-icon.png"> -->
-   <!--  <link rel="shortcut icon" href="favicon.ico"> -->
+    <meta name="google-site-verification" content="NRgkBXOmKw9pm85YTPgV6tzPk4gqL-I1upSntx2UVVo" />
+    <link rel="shortcut icon" href="/public/favicon.ico" type="image/x-icon">
+    <link rel="canonical" href="http://www.freekafanda.in/" />
+    <link rel="icon" href="/public/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="/public/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="/public/css/font-awesome.min.css">
     <!-- <link rel="stylesheet" href="/css/themify-icons.css"> -->
@@ -19,6 +21,15 @@
     <link rel="stylesheet" href="/public/css/toastr.min.css">
     <script src="/public/jquery/jquery-3.2.1.min.js"></script>
     <script src="/public/jquery/jquery.cookie.js"></script>
+    <script data-ad-client="ca-pub-9262832715958252" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-160812913-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-160812913-1');
 </script>
 </head>
 <body>
